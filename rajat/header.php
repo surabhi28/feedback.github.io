4e5b177839e3d16468dbd86c77c3dfd43c384b34
surbhi28 <dakoliya.surabhi@gmail.com>
2017-02-05 12:21:39 +0530

    cc

--- rajat/header.php
+++ rajat/header.php
@@ -16,6 +16,12 @@
     <link href="yamm/yamm.css" rel="stylesheet">
     <link href="css/footer.css" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+    <style>
+  
+  .person:hover {
+      border-color: grey;
+  }
+  </style>
   </head>
   <body>
 
